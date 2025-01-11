492becb35b12e34bf88eff7df27ec46de13432e9
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2025-01-11 10:11:01 +0100

    create variables to hold the dynamic data

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -4,6 +4,7 @@ abstract class StrollColor {
   static const transparent = Colors.transparent;
   static const white = Color(0xFFFFFFFF);
   static const primary = Color(0xFF8B88EF);
+  static const primary100 = Color(0xFFCCC8FF);
   static const dark = Color(0xFF020202);
   static const black50 = Color(0xFF0F1115);
   static const grey = Color(0xFF232A2E);
--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -23,48 +23,114 @@ class _DashboardState extends State<Dashboard> {
     return Container(
       height: MediaQuery.of(context).size.height,
       child: Stack(
-        // fit: StackFit.passthrough,
         children: [
           Container(
-              height: MediaQuery.of(context).size.height * 0.55,
+              height: MediaQuery.of(context).size.height * 0.53,
               width: MediaQuery.of(context).size.width,
               child: Image.asset(
                 'assets/images/video.png',
                 fit: BoxFit.cover,
               )),
           Positioned(
+              top: 100,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Stroll Bonfire',
+                          style: TextStyle(
+                              color: StrollColor.primary.withOpacity(0.98),
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.keyboard_arrow_down,
+                            color: StrollColor.primary.withOpacity(0.98),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Image.asset(
+                              'assets/images/timer.png',
+                              height: 17,
+                              width: 17,
+                            ),
+                            SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              '22h 00m',
+                              style: TextStyle(height: 1, fontSize: 14),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Image.asset(
+                              'assets/images/profile_sm.png',
+                              height: 17,
+                              width: 17,
+                            ),
+                            SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              '103',
+                              style: TextStyle(height: 1, fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )),
+          Positioned(
             bottom: 0,
             child: Column(
               children: [
                 Container(
-                  height: 260,
+                  height: 299,
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                    // border:
-                    //     Border.all(color: StrollColor.dark.withOpacity(0.1)
-                    //     ),
-                    // color: StrollColor.dark,
                     boxShadow: [
                       BoxShadow(
                           color: StrollColor.dark,
-                          blurRadius: 200,
-                          offset: Offset(0, 10)),
+                          blurRadius: 600,
+                          offset: Offset(0, 0)),
                     ],
                   ),
                   child: Stack(
                     clipBehavior: Clip.none,
                     children: [
                       Positioned(
-                        top: -50,
+                        top: -70,
                         child: Container(
                           height: 100,
                           width: MediaQuery.of(context).size.width,
-                          // decoration: BoxDecoration(
-                          //   border: Border.all(color: Colors.blue),
-                          // ),
                           child: ClipRect(
                             child: BackdropFilter(
-                              filter: ImageFilter.blur(sigmaX: 0, sigmaY: 5),
+                              filter: ImageFilter.blur(sigmaX: 0, sigmaY: 10),
                               child: Container(
                                 color: StrollColor.dark.withOpacity(0.1),
                               ),
@@ -73,7 +139,7 @@ class _DashboardState extends State<Dashboard> {
                         ),
                       ),
                       Positioned(
-                        top: -25,
+                        top: -48,
                         child: Container(
                           padding: EdgeInsets.symmetric(horizontal: 10),
                           width: MediaQuery.of(context).size.width,
@@ -166,14 +232,13 @@ class _DashboardState extends State<Dashboard> {
                                       ),
                                     ),
                                     Container(
-                                      height: 200,
+                                      height: 170,
                                       margin: EdgeInsets.only(top: 10),
                                       child: GridView.builder(
                                         padding: EdgeInsets.zero,
                                         gridDelegate:
                                             SliverGridDelegateWithFixedCrossAxisCount(
-                                          crossAxisCount:
-                                              2, // Number of columns
+                                          crossAxisCount: 2,
                                           crossAxisSpacing: 10,
                                           mainAxisSpacing: 10,
                                           childAspectRatio: 4 / 1.6,
@@ -185,6 +250,71 @@ class _DashboardState extends State<Dashboard> {
                                               options[index]['value']!);
                                         },
                                       ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Pick your option.',
+                                              style: TextStyle(
+                                                  fontSize: 13, height: 1.5),
+                                            ),
+                                            Text(
+                                              'See who has a similar mind.',
+                                              style: TextStyle(
+                                                  fontSize: 13, height: 1),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            GestureDetector(
+                                              onTap: () {},
+                                              child: Container(
+                                                  height: 49,
+                                                  width: 49,
+                                                  padding: EdgeInsets.all(10),
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color:
+                                                          StrollColor.primary,
+                                                      width: 2,
+                                                    ),
+                                                  ),
+                                                  child: Image.asset(
+                                                    'assets/images/voice.png',
+                                                  )),
+                                            ),
+                                            const SizedBox(
+                                              width: 10,
+                                            ),
+                                            GestureDetector(
+                                              onTap: () {},
+                                              child: Container(
+                                                  height: 49,
+                                                  width: 49,
+                                                  padding:
+                                                      const EdgeInsets.all(10),
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                          shape:
+                                                              BoxShape.circle,
+                                                          color: StrollColor
+                                                              .primary),
+                                                  child: const Icon(
+                                                    Icons.arrow_forward,
+                                                    color: StrollColor.dark,
+                                                  )),
+                                            ),
+                                          ],
+                                        )
+                                      ],
                                     )
                                   ],
                                 ),
